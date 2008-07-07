35ddcd44d0536a8d168ef6278784e19b3b8a3fef
Tore Darell <toredarell@gmail.com>
2008-07-07 21:13:40 +0200

    Add HAML

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "ruby-openid", :version => '>= 2.0.1', :lib => 'openid'
+  config.gem "haml"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.

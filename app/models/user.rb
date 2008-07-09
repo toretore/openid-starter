200f6466dc79909920c4c78828287fff511dc8a1
Tore Darell <toredarell@gmail.com>
2008-07-09 18:45:14 +0200

    Add display_name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   validates_presence_of :openid_url
   validates_uniqueness_of :openid_url
 
+
+  def display_name
+    name.blank? ? openid_url : name
+  end
+
+
 end
